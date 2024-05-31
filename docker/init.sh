6fc4387afaba0f25c2e533d4d480f46ff1cafeb8
benjamin.enkrich <benjamin.enkrich@gmail.com>
2024-05-31 07:47:10 +0000

    code in /code verschoben, docker-compose angefangen

---
+++ docker/init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Klone das Git-Repository in einen temporären Ordner
+git clone https://github.com/Benkram/photofox /tmp/repo
+
+# Erstelle das Zielverzeichnis, falls es nicht existiert
+mkdir -p ./web
+
+# Kopiere den Inhalt des 'code'-Ordners in das Web-Verzeichnis
+cp -r /tmp/repo/code/* ./web/
+
+# Kopiere die Konfigurationsdateien in das 'configs'-Verzeichnis im Web-Verzeichnis
+mkdir -p ./web/configs
+cp -r ./configs/* ./web/configs/
+
+# Warte, bis die Datenbank bereit ist
+while ! docker exec db mysqladmin --user=root --password=rootpass ping --silent &> /dev/null ; do
+    echo "Warte auf Datenbank..."
+    sleep 2
+done
+
+# Führe die PHP-Datei aus
+docker exec web php /var/www/html/configs/updateSQL.php
+
+# Führe die SQL-Datei auf dem MySQL-Server aus, um die Datenbank zu erstellen
+docker exec -i db mysql -uroot -prootpass < ./web/configs/photofoxDB.sql
+
+# Aufräumen des temporären Repository-Ordners
+rm -rf /tmp/repo
